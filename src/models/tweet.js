558f3022a7725ea7e8a6e7fc553aae86c6c548a9
AshimBarman <ashlabbarman@gmail.com>
2023-06-26 20:53:51 +0530

    removed hashtag array from tweet schema

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -6,12 +6,6 @@ const tweetScema = new mongoose.Schema({
         required: true,
         max: [250, 'Tweet cannot be more than 250 characters']
     },
-    hashTags: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Hashtag'
-        }
-    ]
 
     
 }, {timestamps: true});
